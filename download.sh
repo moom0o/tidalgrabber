1cdbdceb28c779099d2ffce7e994deb14df74e4d
moo <48740106+moom0o@users.noreply.github.com>
2025-11-02 18:15:54 -0500

    example names and check if file exists

--- download.sh
+++ download.sh
@@ -3,7 +3,15 @@ if test -z "$1"; then
 fi
 URL="${1%% *}"
 NAME="${1#* }"
-echo $URL
-echo $NAME
+NAME="${NAME//\//-}"
 mkdir -p ./music
-wget -U "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.127 Safari/537.36" "$URL" -O "./music/${NAME}.flac"
+if [[ ! -f "./music/${NAME}.flac" ]]; then
+    echo "${NAME}"
+    wget -q -U "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.127 Safari/537.36" "$URL" -O "./music/${NAME}.flac"
+    if [[ $? -ne 0 ]]; then
+            echo "  -> Download failed (Server Error). Removing empty file for: ${NAME}.flac"
+            rm -f "${NAME}.flac"
+        else
+            echo "  -> Download successful: ${NAME}.flac"
+        fi
+fi
