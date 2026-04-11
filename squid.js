45f8833b2e9136f589b2c6c956c656eb269a2e1e
moo <48740106+moom0o@users.noreply.github.com>
2026-04-10 21:44:27 -0400

    Update api url

--- squid.js
+++ squid.js
@@ -21,9 +21,8 @@ async function request(string) {
     let args = string.split(' ')
     let name = args.slice(1).join(" ")
     let search = args[0]
-    const res = await unirest('GET', `https://tidal.kinoplus.online/track/?id=${encodeURIComponent(search)}&quality=LOSSLESS`)
-    let url = JSON.parse(Buffer.from(res.body["data"]["manifest"], 'base64').toString('utf-8')).urls[0];
-    if (res.error || !url) {
+    const res = await unirest('GET', `https://triton.squid.wtf/track/?id=${encodeURIComponent(search)}&quality=LOSSLESS`)
+    if (res.error || !res.body["data"]) {
         console.log(res.raw_body);
         if(res.body["detail"] && res.body["detail"] === "Too Many Requests") {
             await request(args)
@@ -33,6 +32,7 @@ async function request(string) {
             });
         }
     } else {
+        let url = JSON.parse(Buffer.from(res.body["data"]["manifest"], 'base64').toString('utf-8')).urls[0];
         console.log(`${url} ${name}`);
         fs.appendFile('./urls.txt', `${url} ${name}` + "\n", (err) => {
             if (err) throw err;
